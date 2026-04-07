c9c6851c6783a5870951f366a6ccd21faa31dd33
hernanveyret <hernanveyret@hotmail.com>
2026-04-06 23:43:24 -0300

    fix css

--- src/componentes/PartidosTerminados.jsx
+++ src/componentes/PartidosTerminados.jsx
@@ -4,6 +4,7 @@ import pelota from '/img/pelota.png';
 const PartidosTerminados = ({ db, setDb }) => {
 const [ juegosFinalizados, setJuegosFinalizados ] = useState(db[0]?.partidosFinalizados ? db[0]?.partidosFinalizados : [])
 const [ bloques, setBloques ] = useState([])
+const [ conteoFaces, setConteoFaces ] = useState([])
 
 useEffect(() => {
   if(db){
@@ -15,15 +16,28 @@ useEffect(() => {
   juegosFinalizados && console.log(juegosFinalizados)
   const gruposDePartidos = juegosFinalizados.map(j => j.jugadas);
 // Para saber cuántos hay en el primero:
-console.log(gruposDePartidos);
-
+//console.log(gruposDePartidos);
+setConteoFaces(gruposDePartidos)
 },[juegosFinalizados])
+
 
   return (
   <div className="bracket-container">
     {juegosFinalizados && juegosFinalizados.map((j, index) => (
       <div key={index} className="columna-fase">
-        <h3 className="titulo-fase">Ronda {index + 1}</h3>
+        <h3 className="titulo-fase">
+          { 
+            conteoFaces?.[index]?.length === 1 ? 
+            'Final' : 
+            conteoFaces?.[index]?.length === 2 ?
+            'Semi final' :
+            conteoFaces?.[index]?.length === 4 ?
+            'Cuartos de final' :
+            conteoFaces?.[index]?.length === 8 ?
+            'Octavos de final' :
+            `Eliminatorias ${index+1}` 
+          }
+        </h3>
         
         <div className="contenedor-lista-bracket">
           {j.jugadas.map((p, i) => (
